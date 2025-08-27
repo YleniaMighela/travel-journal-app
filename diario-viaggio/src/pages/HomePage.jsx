2d386bb5e0bf3784978ba78a48cffeb92f9986ab
Ylenia <yleniamighela@gmail.com>
2025-08-27 12:28:31 +0200

    added basuc layout

---
+++ diario-viaggio/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import PostList from "../components/PostList";
+
+function HomePage() {
+    return (
+        <>
+            <div className="home-header">
+                <h1>Diario di viaggio</h1>
+                <Link to="/form">➕ Aggiungi tappa</Link>
+            </div>
+            <PostList />
+        </>
+    );
+}
+
+export default HomePage;
